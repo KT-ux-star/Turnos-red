60081442f5ed4217e075d89503dbcf3e6cacc4a5
KT-ux-star <kalentornello@gmail.com>
2026-09-08 18:00:10 -0300

    Excluir archivos compilados

--- dist/middlewares/error.handler.d.ts
+++
@@ -1,3 +0,0 @@
-import type { NextFunction, Request, Response } from 'express';
-export declare function errorHandler(error: unknown, _req: Request, res: Response, _next: NextFunction): void;
-//# sourceMappingURL=error.handler.d.ts.map
